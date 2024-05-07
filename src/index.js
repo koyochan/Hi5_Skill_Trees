a110ab678341d22ac38aee9d287b470005358d8f
koyochan <121277745+koyochan@users.noreply.github.com>
2024-05-07 10:04:20 +0900

    delete

--- src/index.js
+++ src/index.js
@@ -65,20 +65,6 @@ const defaultTheme = {
 
 export default defaultTheme;
 
-
-const customTheme = {
-  ...defaultTheme,
-  backgroundColor: '#333',
-  primaryFontColor: '#ccc',
-  nodeActiveBackgroundColor: `linear-gradient(
-      to right,
-      #f06292 0%,
-      #5c6bc0 50%,
-      #26c6da 100%
-    )`,
-};
-
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
